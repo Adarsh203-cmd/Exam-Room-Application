ee86bfa89f17ade1b10b0b1ea5f906e47193a5c2
Dhanraj J Poojary <dhanrajjpoojary25@gmail.com>
2025-04-15 17:35:56 +0530

    added models for exam_content

---
+++ backend/exam_content/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import MCQQuestion, FillInTheBlankQuestion
+
+class MCQQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MCQQuestion
+        fields = '__all__'
+
+class FillInTheBlankSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FillInTheBlankQuestion
+        fields = '__all__'
